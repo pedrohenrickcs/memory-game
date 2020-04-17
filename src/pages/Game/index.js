07c86d4c510d1f91915b8d29bd00c5448d9ddac1
unknown <pedro.henrickcs@gmail.com>
2020-04-17 16:15:02 -0300

    :rocket: initialize hooks

--- src/pages/Game/index.js
+++ src/pages/Game/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import { Container, List, ItemsList, Image } from '../../styles';
 
@@ -6,17 +6,34 @@ import Cards from '../../cards';
 
 export default function Game() {
 
-    // function clickCard () {
+    const [cards, setCards] = useState({
+        noFlipped: false
+    });
+
+    useEffect(() => {
+        setCards(Cards);
+    }, [])
+
+    function handleCard(index) {
+
+        const newCards = Cards.map(c => {
+            return {c, index, noFlipped: true}
+        })
         
-    // }
+        setCards(newCards);
+        console.log('newCards', newCards);
+        
+    }
+
 
     return (
         <Container>
             <List>
-                {Cards.map(card => 
-                <ItemsList> 
-                    <Image src={card} alt=""/>
-                </ItemsList>)}
+                {Cards.map((card, index) => 
+                    <ItemsList key={index} onClick={handleCard}> 
+                        <Image src={card}/>
+                    </ItemsList>)
+                }
             </List>
         </Container>
     )
